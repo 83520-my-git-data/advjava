d3bf4feadafb5e7053df0f70ce906673317cf7cb
kd4-hemant-sahu-83520 <rkhemant100@gmail.com>
2024-06-09 21:35:40 +0530

    assignment uploaded

---
+++ assignment02_classwork/day02/Election/src/main/java/com/sunbeam/servlets/VoteServlet.java
@@ -0,0 +1,129 @@
+package com.sunbeam.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sunbeam.daos.CandidateDao;
+import com.sunbeam.daos.CandidateDaoImpl;
+import com.sunbeam.daos.UserDao;
+import com.sunbeam.daos.UserDaoImpl;
+import com.sunbeam.pojos.User;
+
+public class VoteServlet extends HttpServlet
+{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		process(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		process(req, resp);
+	}
+	
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		
+//		String candidateId = req.getParameter("candidate");
+//		int id = Integer.parseInt(candidateId);
+//		
+//		try(CandidateDao candDao = new CandidateDaoImpl()){
+//			candDao.incrementVote(id);
+//		}
+//		catch(Exception e)
+//		{
+//			e.printStackTrace();
+//			throw new ServletException(e);
+//		}
+		
+		
+		
+		
+		HttpSession session = req.getSession();
+		
+		User user1 = (User) session.getAttribute("curuser");
+		
+		int userId = user1.getId();
+		
+		String message="";
+		
+		if(user1.getStatus() == 0)
+		{
+		
+		String candidateId = req.getParameter("candidate");
+		int id = Integer.parseInt(candidateId);
+		
+		try(CandidateDao candDao = new CandidateDaoImpl()){
+			candDao.incrementVote(id);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+			message = "Your vote is registered successfully... <br/><br/>";
+			
+			
+			try(UserDao userDao = new UserDaoImpl()) 
+			{
+				userDao.updateStatus(userId, true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new ServletException(e);
+			}
+			
+		}else {
+			message= "You have already voted. <br/><br/>";
+		}
+		
+		
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+ 		out.println("<title>Voted</title>");
+		out.println("</head>");
+		out.println("<body align='center' bgcolor='pink'>");
+		
+		String userName="";
+		Cookie[] arr = req.getCookies();
+		
+		if(arr != null)
+		{
+			for(Cookie c : arr)
+			{
+				if(c.getName().equals("uname"))
+				{
+					userName = c.getValue();
+					break;
+				}
+			}   
+		}
+		out.printf("Hello  %s<hr/>\n", userName);
+ 		
+		
+		
+		//out.println("Your vote has been registered successfully...");
+		out.println(message);
+		out.println("<a href='logout'>Sign Out</a>");
+ 		out.println("</body>");		
+		out.println("</html>");
+	}
+	
+	
+
+}
